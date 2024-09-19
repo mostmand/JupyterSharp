ec6558e9053fe7924f762f119b0e56d3668767ad
mostmand <mostmand.mh@gmail.com>
2024-09-20 00:45:52 +0330

    Add DependencyInjection

---
+++ src/JupyterSharp/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace JupyterSharp;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddJupyterClient(this IServiceCollection services)
+    {
+        return services;
+    }
+}
