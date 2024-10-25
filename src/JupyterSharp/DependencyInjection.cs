2036f0bb305604cf68e3bda45856cc4049e756db
mostmand <mostmand.mh@gmail.com>
2024-10-26 01:40:40 +0330

    Add JupyterClientFactory.cs

--- src/JupyterSharp/DependencyInjection.cs
+++ src/JupyterSharp/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using JupyterSharp.Abstraction;
+﻿using System;
+using JupyterSharp.Abstraction;
 using JupyterSharp.Client;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -6,7 +7,7 @@ namespace JupyterSharp;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddJupyterClient(this IServiceCollection services)
+    public static IServiceCollection AddJupyterClient(this IServiceCollection services, Action<IJupyterOptionsBuilder> options)
     {
         services.AddSingleton<IJupyterClient, JupyterClient>();
         
---
+++ src/JupyterSharp/JupyterClientFactory.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JupyterSharp;
+
+public sealed class JupyterClientFactory : IDisposable, IAsyncDisposable
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly bool _disposeServiceProvider;
+
+    internal JupyterClientFactory(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public JupyterClientFactory(Action<IJupyterOptionsBuilder> options)
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddJupyterClient(options);
+        _serviceProvider = serviceCollection.BuildServiceProvider();
+        _disposeServiceProvider = true;
+    }
+
+    public void Dispose()
+    {
+        if (!_disposeServiceProvider)
+        {
+            return;
+        }
+
+        if (_serviceProvider is not IDisposable disposable)
+        {
+            return;
+        }
+
+        disposable.Dispose();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (!_disposeServiceProvider)
+        {
+            return;
+        }
+
+        if (_serviceProvider is not IAsyncDisposable asyncDisposable)
+        {
+            return;
+        }
+
+        await asyncDisposable.DisposeAsync().ConfigureAwait(false);
+    }
+}
