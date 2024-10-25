2299e24f0e575004ef341b34f0b075cbda3b0a61
mostmand <mostmand.mh@gmail.com>
2024-10-26 01:25:11 +0330

    Add integration test project

---
+++ test/JupyterSharp.Integration.Test/SimpleGetJupyterServerVersionTest.cs
@@ -0,0 +1,11 @@
+﻿using Xunit;
+
+namespace JupyterSharp.Integration.Test;
+
+public class SimpleGetJupyterServerVersionTest
+{
+    [Fact]
+    public void GetJupyterServerVersion_ShouldReturnJupyterServerVersion()
+    {
+    }
+}
